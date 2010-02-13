d443dc7c7450b407b1135cb32d1d13a5da89afd4
daniell <daniell@aptivate.org>
2010-02-13 15:49:55 +0000

    modified:   mapfiles/openlayers.html modified:   mapsearch.php Pages produced by these files now pass web validation tests.

--- mapsearch.php
+++ mapsearch.php
@@ -24,15 +24,23 @@
   		$feed_url = "http://www.reliefweb.int/RWFeed/FastRSS?fql=and%28meta.collection%3Aor%28rwmaps%29%2Crwcc%3Ahti%2Crwrc%3A2%2Crwarchived%3Anot%28%271%27%29%2Cstring%28%22IDP%22%2C+mode%3D%22simpleall%22%2Cannotation_class%3D%22user%22%29%29&view=rwallsppublished&hits=25&offset=0&qtf_lemmatize=1&sortby=rwpubdate-rwpubdatedisplay&sortdirection=descending&collapseon=batvuigeneric1"; 
 		break;
   	default: 
-  		$feed_url = "http://www.reliefweb.int/RWFeed/FastRSS?fql=and%28meta.collection%3Aor%28rwmaps%29%2Crwcc%3Ahti%2Crwrc%3A2%2Crwarchived%3Anot%28%271%27%29%2Cstring%28%22%22%2C+mode%3D%22simpleall%22%2Cannotation_class%3D%22user%22%29%29&view=rwallsppublished&hits=50&offset=0&qtf_lemmatize=1&sortby=rwpubdate-rwpubdatedisplay&sortdirection=descending&collapseon=batvuigeneric1"; 
+  		$feed_url = "http://www.reliefweb.int/RWFeed/FastRSS?fql=and%28meta.collection%3Aor%28rwmaps%29%2Crwcc%3Ahti%2Crwrc%3A2%2Crwarchived%3Anot%28%271%27%29%2Cstring%28%22%22%2C+mode%3D%22simpleall%22%2Cannotation_class%3D%22user%22%29%29&view=rwallsppublished&hits=25&offset=0&qtf_lemmatize=1&sortby=rwpubdate-rwpubdatedisplay&sortdirection=descending&collapseon=batvuigeneric1"; 
 		break;
-  } 
-?> 
-<html> 
+  }  
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+		<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+		<?php
+			echo "<title>Haiti " . $search_string . " search</title>" 
+		?>
+	</head> 
 	<body>
-  		<ul> 
 <?php   
-  echo $search_string;
+  echo "<h1>Haiti " . $search_string . " search</h1>";
+  echo "<ul>"; 
   $RSS_PHP->load($feed_url); 
   foreach($RSS_PHP->items as $item) 
   { 
@@ -54,17 +62,17 @@
       echo "<li>";
       print_r($item['title']['value']);
       echo " (<a href=\"" . $localuntiledfile . "\">PDF</a>) (no images yet)";
-      echo "</li><br/>";
+      echo "</li>";
     }
     if (file_exists($localtiledfile))
     {
       echo "<li>";
       print_r($item['title']['value']);
       echo " (<a href=\"" . $localtiledfile . "\">PDF</a>) (<a href=\"" . $localtileviewer . "\">images</a>)";
-      echo "</li><br/>";
+      echo "</li>";
     }
   } 
-?> 
-		</ul> 
+  echo "</ul>";
+?> 		 
 	</body> 
 </html>
