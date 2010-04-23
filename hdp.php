cd29dc21e09a4dac8bd4adb4b037273e0bfba3ea
daniell <daniell@aptivate.org>
2010-04-23 10:04:54 +0000

    Added files to allow creation and searching of HDP database.

---
+++ hdp.php
@@ -0,0 +1,219 @@
+<?php
+
+//Connect to database
+mysql_connect("localhost","root","idealp") or die(mysql_error());
+//mysql_connect("localhost","root","3r4frm") or die(mysql_error());
+mysql_select_db("reliefweb") or die(mysql_error());
+
+//Check input values
+$regionid = "";
+$condition = "";
+$terms = "";
+if(isset($_GET['country']) && $_GET['country'] !="")
+{
+	$country = $_GET['country'];
+	$result = mysql_query("SELECT id FROM country WHERE name='".$country."'") or die(mysql_error());
+	$row = mysql_fetch_array($result);
+	$countryid = $row['id'];
+	$condition = $condition."countryid = '".$countryid."'";
+}
+if(isset($_GET['region']) && $_GET['region'] != "")
+{
+	$region = $_GET['region'];
+	$result = mysql_query('SELECT id FROM region WHERE name="'.$region.'"') or die(mysql_error());
+	$row = mysql_fetch_array($result);
+	$regionid = $row['id'];
+	if($condition != "")
+	{
+		$condition .= " and ";
+	}
+	$condition = $condition."regionid = '".$regionid."'";
+}
+if(isset($_GET['datacategory'])&& $_GET['datacategory'] != "")
+{
+	$dataCategory = $_GET['datacategory'];
+	$result = mysql_query('SELECT id FROM dataCategory WHERE name="'.$dataCategory.'"') or die(mysql_error());
+	$row = mysql_fetch_array($result);
+	$dataCategoryid = $row['id'];
+	if($condition != "")
+	{
+		$condition .= " and ";
+	}
+	$condition = $condition."dataCategoryid = '".$dataCategoryid."'";
+}
+if(isset($_GET['datatype'])&& $_GET['datatype'] != "")
+{
+	$dataType = $_GET['datatype'];
+	$result = mysql_query('SELECT id FROM dataType WHERE name="'.$dataType.'"') or die(mysql_error());
+	$row = mysql_fetch_array($result);
+	$dataTypeid = $row['id'];
+	if($condition != "")
+	{
+		$condition .= " and ";
+	}
+	$condition = $condition."dataTypeid = '".$dataTypeid."'";
+}
+if(isset($_GET['cluster'])&& $_GET['cluster'] != "")
+{
+	$cluster = $_GET['cluster'];
+	$result = mysql_query('SELECT id FROM cluster WHERE name="'.$cluster.'"') or die(mysql_error());
+	$row = mysql_fetch_array($result);
+	$clusterid = $row['id'];
+	if($condition != "")
+	{
+		$condition .= " and ";
+	}
+	$condition = $condition."clusterid = '".$clusterid."'";
+}
+//HTML top
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+<title>Humanitarian Data Portal</title>
+</head>
+<body>
+
+
+<!-- Display search form for inventory items that match the input values -->
+<form method="get" action="hdp.php">
+ Region:
+ <select name="region">
+ <?php
+$results = mysql_query('SELECT name FROM region') or die(mysql_error());
+echo '<option value=""></option>';
+while($theResult = mysql_fetch_array($results))
+{
+	if ($theResult['name'] == $_GET['region']) $selected = " selected"; else $selected = "";
+	echo '<option value="'.$theResult['name'].'"'.$selected.'>'.$theResult['name'].'</option>';
+}
+?>
+</select>
+<br />Country:
+<select name="country">
+<?php
+if ($regionid != "") $where = ' where regionid = "'.$regionid.'"'; else $where = "";
+$results = mysql_query('SELECT name FROM country'.$where) or die(mysql_error());
+echo '<option value=""></option>';
+while($theResult = mysql_fetch_array($results))
+{   
+	if ($theResult['name'] == $_GET['country']) $selected = " selected"; else $selected = "";
+	echo '<option value="'.$theResult['name'].'"'.$selected.'>'.$theResult['name'].'</option>';
+}?>
+</select>
+<br />Data Category:
+<select name="datacategory">
+<?php
+$results = mysql_query('SELECT name FROM dataCategory') or die(mysql_error());
+echo '<option value=""></option>';
+while($theResult = mysql_fetch_array($results))
+{
+	if ($theResult['name'] == $_GET['datacategory']) $selected = " selected"; else $selected = "";
+	echo '<option value="'.$theResult['name'].'"'.$selected.'>'.$theResult['name'].'</option>';
+}
+?>
+</select>
+<br />Data Type:
+<select name="datatype">
+<?php
+$results = mysql_query('SELECT name FROM dataType') or die(mysql_error());
+echo '<option value=""></option>';
+while($theResult = mysql_fetch_array($results))
+{   
+	if ($theResult['name'] == $_GET['datatype']) $selected = " selected"; else $selected = "";
+	echo '<option value="'.$theResult['name'].'"'.$selected.'>'.$theResult['name'].'</option>';
+}?>
+</select>
+<br />Cluster:
+<select name="cluster">
+<?php
+$results = mysql_query('SELECT name FROM cluster') or die(mysql_error());
+echo '<option value=""></option>';
+while($theResult = mysql_fetch_array($results))
+{
+	if ($theResult['name'] == $_GET['cluster']) $selected = " selected"; else $selected = "";
+	echo '<option value="'.$theResult['name'].'"'.$selected.'>'.$theResult['name'].'</option>';
+}?>
+</select>
+<br /><input type="submit" value="Search">
+</form>
+
+<?php
+//print the output, with search terms removed and a clickable name for each line
+$foundName = "";
+if($condition != "")
+{
+	//Search database
+	$results2 = mysql_query('SELECT * FROM inventory WHERE '.$condition) or die(mysql_error());
+?>
+	<table border="1">
+	<tr>
+	<th>Data Name</th>
+	<th>Data Access</th>
+	<th>Access Level</th>
+	<th>Data Type</th>
+	<th>Data Category</th>
+	<th>Data Indicator</th>
+	<th>Cluster</th>
+	<th>Data Format</th>
+	<th>Data Region</th>
+	<th>Data Country</th>
+	<th>Source</th>
+	<th>Resolution</th>
+	</tr>
+<?php
+	while($theResult = mysql_fetch_array($results2))
+	{
+		echo "<tr>";
+		echo '<td><a href="./hdp.php?dataname='.$theResult['id'].'">'.$theResult['dataName'].'</a></td>';
+		echo '<td><a href="'.$theResult['dataAccess'].'">website</a></td>';
+		$queryData = mysql_query('SELECT name FROM access WHERE id = '.$theResult['accessid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM dataType WHERE id = '.$theResult['dataTypeid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM dataCategory WHERE id = '.$theResult['dataCategoryid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM dataIndicator WHERE id = '.$theResult['dataIndicatorid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM cluster WHERE id = '.$theResult['clusterid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM dataFormat WHERE id = '.$theResult['dataFormatid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM region WHERE id = '.$theResult['regionid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT name FROM country WHERE id = '.$theResult['countryid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['name']."</td>";
+		$queryData = mysql_query('SELECT shortName FROM source WHERE id = '.$theResult['sourceid']) or die(mysql_error()); 
+		$row = mysql_fetch_array($queryData);
+		echo "<td>".$row['shortName']."</td>";
+		echo "<td>".$theResult['resolution']."</td>";
+	}
+
+	//Show these if list clicked on: pointOfContact,processingInstructions,notes
+	echo "</table>";
+}
+
+//HTML Footer
+if (isset($_GET['dataname']) && $_GET['dataname'] != "")
+{
+	$results2 = mysql_query('SELECT * FROM inventory WHERE id="'.$_GET['dataname'].'"') or die(mysql_error());
+	$row = mysql_fetch_array($results2);
+	echo "<p><b>Data name:</b> ".$row['dataName'].'</p>';
+	echo "<p><b>Point of Contact:</b> ".$row['pointOfContact'].'</p>';
+	echo "<p><b>Processing Instructions:</b> ".$row['processingInstructions'].'</p>';
+	echo "<p><b>Notes:</b> ".$row['notes'].'</p>';
+}
+?>
+
+</body>
+</html>
